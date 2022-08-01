dd1bf4e3e6bac70e1138d40fb4101f1c25db3ea3
Mohamed Ashraf <mohamed.hafez4050@gmail.com>
2022-08-01 22:57:28 +0200

    writeInTextInFile

--- Main/main.py
+++ Main/main.py
@@ -1,50 +1,35 @@
 import cv2
 import pytesseract
 import glob, os
-import xlwt
-from xlwt import Workbook
 
-wb = Workbook()
-sheet1 = wb.add_sheet('Sheet 1')
 
-def getAllImagesINFolder(folder):
+
+def getPathOfImagesINFolder(folderPath):
     images = []
-    for dirpath,_,filenames in os.walk(folder):
+    for dirpath,_,filenames in os.walk(folderPath):
         for filename in filenames:
             if filename.endswith(".jpg"):
                 images.append(os.path.abspath(os.path.join(dirpath, filename)))
     return images
 
-def procces(path):
+def getTextInImage(imagePath):
     pytesseract.pytesseract.tesseract_cmd = r"C:\Tesseract-OCR\tesseract.exe"
-    img = cv2.imread(path)
+    img = cv2.imread(imagePath)
     #cv2.imshow("Image", img)
     return pytesseract.image_to_string(img)
 
-# def excelWrite(dic, data,index: int):
-#     for i in range(len(dic)):
-        
 
+def writeTextInFile():
+    f = open("demofile2.txt", "a")
+    for images in getPathOfImagesINFolder("./images"):
+        f.write(getTextInImage(images))
+    f.close()
 
-# parametersCount = int(input("parameters Count"))
-
-# dic=[];
-# for x in range(parametersCount):
-#     first = input("first Text");
-#     second = input("2nd Text");
-#     patameter = input("parameter name");
-#     dic.append({"first":first, "2nd":second,"patameter":patameter})
-
-for images in getAllImagesINFolder("./images"):
-    print(images)
-    print(procces(images))
-    print("==============================================================================")
-
-
-
-# def extract( start ,  end, data):
-#    return (data.split(start))[1].split(end)[0]
-
+    
+writeTextInFile()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
